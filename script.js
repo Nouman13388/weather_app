36405e623a19c18b94eefa2f31df5e98b6c52af3
Nouman13388 <nouman.nomi1338@gmail.com>
2025-03-11 16:48:19 +0500

    weather card addition
    added weather card

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 function getWeather() {
-    const city = document.getElementById('search-bar').value;
-    const key = 'f527ddffd52e46f286372143250703';
-    const days = 1;
-    const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&days=${days}`;
+  const city = document.getElementById("search-bar").value;
+  const key = "f527ddffd52e46f286372143250703";
+  const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`;
 
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            if (data && data.current) {
-                document.getElementById('temperature').textContent = `${data.current.temp_c}°C`;
-                document.getElementById('description').textContent = data.current.condition.text;
-                document.getElementById('location').textContent = `${data.location.name}, ${data.location.region}`;
-                const weatherIcon = document.getElementById('weather-icon');
-                weatherIcon.src = data.current.condition.icon;
-                weatherIcon.alt = data.current.condition.text;
-            } else {
-                alert("Weather data could not be fetched. Please try again.");
-            }
-        })
-        .catch((error) => {
-            console.error("Error fetching weather data:", error);
-            alert("Error fetching weather data. Please try again.");
-        });
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data && data.current) {
+        const weatherCard = document.createElement("div");
+        weatherCard.id = "weather-card";
+        weatherCard.innerHTML = `
+            <img class="weather-icon" src="" alt="">
+            <div class="temperature"></div>
+            <div class="description"></div>
+            <div class="location"></div>
+          `;
+        const weatherContainer = document.querySelector(".weather-container");
+
+        weatherContainer.appendChild(weatherCard);
+
+        weatherCard.querySelector(
+          ".temperature"
+        ).textContent = `${data.current.temp_c}°C`;
+        weatherCard.querySelector(".description").textContent =
+          data.current.condition.text;
+        weatherCard.querySelector(
+          ".location"
+        ).textContent = `${data.location.name}, ${data.location.region}`;
+
+        const weatherIcon = weatherCard.querySelector(".weather-icon");
+        weatherIcon.src = data.current.condition.icon;
+        weatherIcon.alt = data.current.condition.text;
+      } else {
+        alert("Weather data could not be fetched. Please try again.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching weather data:", error);
+      alert("Error fetching weather data. Please try again.");
+    });
 }
